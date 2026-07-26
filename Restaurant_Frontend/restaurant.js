16a173d28842bd0715fcd6561d93ccd5877d244e
Rachael Glazner <ragl8391@colorado.edu>
2026-07-26 19:57:18 +0000

    Restaurant location HTML and javascript files created

---
+++ Restaurant_Frontend/restaurant.js
@@ -0,0 +1,34 @@
+// Elements
+const VerifyBtn = document.getElementById("verify-btn");
+const AddressInput = document.getElementById("restaurant_address");
+const MapDisplay = document.getElementById("map-display");
+
+// Listen for button click to verify address
+VerifyBtn.addEventListener("click", verifyAddress);
+
+function verifyAddress() {
+    const address = AddressInput.value.trim();
+
+    if (address == "") {
+        alert("Please enter an address.");
+        return;
+    }
+
+    // Test verification
+    MapDisplay.innerHTML = `<p>Verifying: ${address}</p>`;
+
+    // Encode verified address
+    const EncodedAddress = encodeURIComponent(address);
+
+    // Display address using Google Maps
+    MapDisplay.innerHTML = `
+    <iframe
+        width="100%"
+        height="300"
+        style="border:0;"
+        loading="lazy"
+        src="https://www.google.com/maps?q=${EncodedAddress}&output=embed">
+    </iframe>
+    `;
+}
+    
